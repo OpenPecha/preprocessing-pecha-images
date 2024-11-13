6c6ea959c0ea9c07a49d15cd729940709c05c5cd
tenkalden <tenkalden007@gmail.com>
2024-11-13 11:59:16 +0530

    added script for preprocessing

---
+++ src/preprocessing_images/preprocess.py
@@ -0,0 +1,39 @@
+import cv2
+import os
+
+
+def preprocess_image(image_path):
+    """
+    Preprocesses an image by converting it to grayscale.
+
+    Args:
+        image_path (str): The file path to the image to be processed.
+
+    Returns:
+        numpy.ndarray: The processed grayscale image.
+    """
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    return gray
+
+
+def process_images_in_directory(input_directory, output_directory):
+    if not os.path.exists(output_directory):
+        os.makedirs(output_directory)
+    for filename in os.listdir(input_directory):
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(input_directory, filename)
+            processed_image = preprocess_image(image_path)
+            processed_image_path = os.path.join(output_directory, "processed_" + filename)
+            cv2.imwrite(processed_image_path, processed_image)
+            print(f"Processed image saved at: {processed_image_path}")
+
+
+def main():
+    input_directory = 'data/pecing/W1KG13126'
+    output_directory = 'data/preprocessed_images'
+    process_images_in_directory(input_directory, output_directory)
+
+
+if __name__ == '__main__':
+    main()
--- tests/test_example.py
+++ tests/test_example.py
@@ -1,4 +1,4 @@
-from project_name.example import add_one
+from preprocessing_images.preprocess import add_one
 
 
 def test_add_one():
